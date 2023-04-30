b9eb4a85579c7b6184bfbf923e6af2475b9d804d
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-04-29 23:28:46 -0300

    upd: tratando erro 500

--- src/main/kotlin/br/com/jpdev/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/jpdev/forum/exception/ExceptionHandler.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.lang.Exception
 import javax.servlet.http.HttpServletRequest
 
 @RestControllerAdvice
@@ -19,6 +20,20 @@ class ExceptionHandler {
         return ErrorView(
                 status = HttpStatus.NOT_FOUND.value(),
                 error = HttpStatus.NOT_FOUND.name,
+                message = exception.message,
+                path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleServerError(
+            exception: NotFoundException,
+            request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+                status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                error = HttpStatus.INTERNAL_SERVER_ERROR.name,
                 message = exception.message,
                 path = request.servletPath
         )
