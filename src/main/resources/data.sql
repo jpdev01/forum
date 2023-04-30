45868460bf9abf5600894b2a109500bd1de00073
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-04-30 16:12:13 -0300

    upd: usando db migrations para ter um controle mais fino do banco

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into course(id, name, category) values(1, 'Kotlin', 'Programacao');
-insert into user(id, name, email) values(1, 'Ana', 'ana@gmail.com')
---
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -0,0 +1,48 @@
+create table course
+(
+    id       bigint not null,
+    name     varchar(50),
+    category varchar(50),
+    primary key (id)
+);
+
+insert into course(id, name, category) values(1, 'Kotlin', 'Programacao');
+
+create table user
+(
+    id    bigint not null,
+    name  varchar(50),
+    email varchar(50),
+    primary key (id)
+);
+
+insert into user(id, name, email) values(1, 'Ana', 'ana@gmail.com');
+
+create table topic
+(
+    id bigint not null,
+    title varchar(50) not null,
+    message varchar(300),
+    date_created datetime not null,
+    status varchar(20) not null,
+    course_id bigint not null,
+    author_id bigint not null,
+
+    primary key (id),
+    foreign key (course_id) references course (id),
+    foreign key (author_id) references user (id)
+);
+
+create table answer
+(
+    id bigint not null,
+    message varchar(300),
+    date_created datetime not null,
+    topic_id bigint not null,
+    author_id bigint not null,
+    solve_topic int(1) not null,
+
+    primary key(id),
+    foreign key(topic_id) references topic(id),
+    foreign key(author_id) references user(id)
+);
