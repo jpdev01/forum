cdf7191c7a1fd2c8f4de978cb24a479db8c5cdf3
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-04-29 23:33:06 -0300

    upd: custom message validation

--- src/main/kotlin/br/com/jpdev/forum/dto/SaveTopicRequestForm.kt
+++ src/main/kotlin/br/com/jpdev/forum/dto/SaveTopicRequestForm.kt
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class SaveTopicRequestForm(
-        @field:NotEmpty @field:Size(min = 5, max = 100) val title: String,
+        @field:NotEmpty @field:Size(min = 5, max = 100, message = "O título deve ter entre 5 e 100 caracteres") val title: String,
         @field:NotEmpty val message: String,
         @field:NotNull val courseId: Long,
         @field:NotNull val authorId: Long
