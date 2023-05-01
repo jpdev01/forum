7241934809744657d59c89dab6d3bd783b64671c
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-01 19:27:25 -0300

    feat: configurando security

---
+++ src/main/kotlin/br/com/jpdev/forum/config/SecurityConfiguration.kt
@@ -0,0 +1,33 @@
+package br.com.jpdev.forum.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration(
+        private val userDetailsService: UserDetailsService
+) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(web: WebSecurity?) {
+        http?.authorizeRequests()?.anyRequest()?.authenticated()?. // qualquer requisição precisa estar autenticada
+        and()?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?. // Nao quero que minha app nao guarde estado de autenticação
+        and()?.formLogin()?.disable()?.httpBasic() // sem tela de login
+    }
+
+    @Bean // para ser gerenciado pelo Spring
+    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth?.userDetailsService(userDetailsService)?.passwordEncoder(bCryptPasswordEncoder())
+    }
+}
---
+++ src/main/resources/db/migration/V2_alter_table_user_add_password.sql
@@ -0,0 +1,1 @@
+ALTER TABLE user add column password text;
